82620f6c9326db5cd5c7700678f2f5ad41e83706
Kelly <kelly@kelredd.com>
2011-08-15 22:20:27 -0500

    update irb test helper

--- test/irb.rb
+++ test/irb.rb
@@ -1,5 +1,3 @@
-require 'test_belt'
-
 # this file is required in when the 'irb' rake test is run.
 # b/c 'test_belt' is required above, lib and test dirs will
 # be added to the LOAD_PATH and the test helper will be
