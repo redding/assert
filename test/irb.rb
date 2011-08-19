8197070198c4736c5c7d1a813e1243bddd5e4c20
Kelly <kelly@kelredd.com>
2011-08-19 12:18:59 -0500

    updated test/irb.rb for new setup workflow

--- test/irb.rb
+++ test/irb.rb
@@ -1,6 +1,5 @@
 # require in any test helper and load user settings
 require 'assert/setup'
-Assert::Helpers.load(caller)
 
 # this file is required in when the 'irb' rake test is run.
 # put any IRB setup code here
