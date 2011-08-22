75b19b4343ee642eaef5822d58780c45ab5d252a
Kelly <kelly@kelredd.com>
2011-08-22 14:02:45 -0500

    removing legacy examples for now

--- examples/empty_test.rb
+++
@@ -1,5 +0,0 @@
-require 'assert'
-
-class EmptyTest < Assert::Context
-
-end
--- examples/single_test.rb
+++
@@ -1,9 +0,0 @@
-require 'assert'
-
-class SingleTest < Assert::Context
-
-  def test_this_thing
-    assert 1==1
-  end
-
-end
