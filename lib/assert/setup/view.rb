5e001e2abce7a57e97e9fc5d7ddac87bfc58d14b
Kelly <kelly@kelredd.com>
2011-09-08 15:13:34 -0500

    updating assert to use DefaultView in assert-view:
    * see pull request on assert-view: https://github.com/teaminsight/assert-view/pull/3

--- lib/assert/setup/view.rb
+++ lib/assert/setup/view.rb
@@ -1,29 +1,11 @@
-require 'assert/view/terminal'
-require 'assert/view/helpers/ansi'
+require 'assert/view/default_view'
 
 module Assert
 
-  # define an Assert::View:Terminal view that renders ansi output using the
-  # 'assert.ansi' template and setting up some styling defaults
-  class AnsiTerminal < View::Terminal
-
-    helper View::Helpers::AnsiStyles
-    options do
-      default_template        'assert.ansi'
-      default_styled          false
-      default_passed_styles   :green
-      default_failed_styles   :red, :bold
-      default_errored_styles  :yellow, :bold
-      default_skipped_styles  :cyan
-      default_ignored_styles  :magenta
-    end
-
-  end
-
-  # Setup the above view, rendering on $stdout, as the default view for assert
+  # Setup the default view, rendering on $stdout
   # (override in user or package helpers)
   options do
-    default_view AnsiTerminal.new($stdout)
+    default_view View::DefaultView.new($stdout)
   end
 
   # setup the global Assert.view method
--- test/default_view_test.rb
+++ test/default_view_test.rb
@@ -7,36 +7,8 @@ module Assert
     desc "assert's default view"
     subject { Assert.options.default_view }
 
-    should "be the AnsiTerminal" do
-      assert_kind_of AnsiTerminal, subject
-    end
-
-  end
-
-  class OptionsTests < DefaultViewTests
-    desc "options"
-    subject do
-      Assert.options.default_view.options
-    end
-
-    should "be an Options::Base object" do
-      assert_kind_of Assert::Options::Base, subject
-    end
-
-    should "default the template" do
-      assert_equal 'assert.ansi', subject.default_template
-    end
-
-    should "default the styled option" do
-      assert_equal false, subject.default_styled
-    end
-
-    should "default its result styles" do
-      assert_equal :green, subject.default_passed_styles
-      assert_equal [:red, :bold], subject.default_failed_styles
-      assert_equal :magenta, subject.default_ignored_styles
-      assert_equal :cyan, subject.default_skipped_styles
-      assert_equal [:yellow, :bold], subject.default_errored_styles
+    should "be the DefaultView" do
+      assert_kind_of View::DefaultView, subject
     end
 
   end
