e62bf85165cd03b6542bdd3320085941a04abcac
Kelly <kelly@kelredd.com>
2011-09-14 16:21:05 -0500

    updated the handler to create a test handler

--- lib/assert/rake_tasks/handler.rb
+++ lib/assert/rake_tasks/handler.rb
@@ -1,10 +1,16 @@
+require 'assert/rake_tasks/test_task'
 require 'assert/rake_tasks/irb'
+require 'assert/rake_tasks/tests'
 
 module Assert::RakeTasks
   class Handler
 
     def self.irb(path)
       Irb.new(path)
+    end
+
+    def self.tests(path)
+      Tests.new(path)
     end
 
   end
---
+++ lib/assert/rake_tasks/tests.rb
@@ -0,0 +1,9 @@
+module Assert::RakeTasks
+  class Tests
+
+    def initialize(test_root)
+      @test_root = test_root
+    end
+
+  end
+end
