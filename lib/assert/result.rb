redding/assert

commit 2538fc2008511b54d9bb35db2aff16ccc0a8a8c5

properly handle skip backtraces
There were a couple holes I discovered while debugging the backtrace
problems I saw in using the TODO skip in #130.

First, the skip result wasn't checking its context info `called_from`
when building its trace value.  Fail results do this so the macro
fails show the correct result.  Skips need to do this as well since
they can be generated from a context caller like fail results from
a macro.

Second, the context info wasn't properly building its `called_from`.
The first caller should be used as the called from if no called
from is explicitly provided.  What's funny is it seems the tests
were originally written with this intent, just not implmented
properly.

lib/assert/result.rb
@@ -135,6 +135,11 @@ module Assert::Result
       "Skip"
     end
 
+    # override of the base, show the test's context info called_from
+    def trace
+      self.test.context_info.called_from || super
+    end
+
   end
 
   class Error < Base

lib/assert/context.rb
@@ -122,7 +122,7 @@ module Assert
           # create a test from the given code block
           Assert.suite.tests << Test.new(test_name, ci, &block)
         else
-          test_eventually(desc_or_macro, called_from, first_caller, &block)
+          test_eventually(desc_or_macro, called_from, first_caller || caller.first, &block)
         end
       end
 

lib/assert/suite.rb
@@ -4,18 +4,13 @@ module Assert
   class Suite
 
     class ContextInfo
-
-      attr_reader :called_from, :first_caller, :klass, :file
+      attr_reader :called_from, :klass, :file
 
       def initialize(klass, called_from=nil, first_caller=nil)
-        @first_caller = first_caller
-        @called_from = called_from
+        @called_from = called_from || first_caller
         @klass = klass
-        @file = if (@called_from || @first_caller)
-          (@called_from || @first_caller).gsub(/\:[0-9]+.*$/, '')
-        end
+        @file = @called_from.gsub(/\:[0-9]+.*$/, '') if @called_from
       end
-
     end
 
     TEST_METHOD_REGEX = /^test./

test/unit/suite_tests.rb
@@ -181,14 +181,18 @@ class Assert::Suite
     end
     subject{ @info }
 
-    should have_readers :called_from, :first_caller, :klass, :file
+    should have_readers :called_from, :klass, :file
 
     should "set its klass on init" do
       assert_equal @klass, subject.klass
     end
 
-    should "set its called_from to the first caller on init" do
-      assert_equal @caller.first, subject.first_caller
+    should "set its called_from to the called_from or first caller on init" do
+      info = Assert::Suite::ContextInfo.new(@klass, @caller.first, nil)
+      assert_equal @caller.first, info.called_from
+
+      info = Assert::Suite::ContextInfo.new(@klass, nil, @caller.first)
+      assert_equal @caller.first, info.called_from
     end
 
     should "set its file from caller info on init" do

commit a70d5449d956a48e370ce647c190951a8b3e39f8

properly determine a result trace line
This updates the logic to prefer the first filtered backtrace line
and then fallback to where the test was called from (in its context
info).  This properly sets the trace line for results not generated
in a test code block (like macros or tests with no block).

This fixes a regression in 2538fc2008511b54d9bb35db2aff16ccc0a8a8c5
that caused the called from to be shown over the backtrace for _all_
fail results.

The net effect of this new behavior is we expect filtered backtraces
to be empty at times and that triggers the called from fallback.  This
greatly simplifies the filtering logic on the backtrace.

Finally, this adds the assert bin path to the filter out logic.  Since
the bin was introduced in v2.0.0, assert hasn't been filtering out
assert lines in the backtrace.  This properly fixes that.

lib/assert/result.rb
@@ -49,7 +49,7 @@ module Assert::Result
     end
 
     def trace
-      self.backtrace.filtered.first.to_s
+      (self.backtrace.filtered.first || self.test.context_info.called_from).to_s
     end
 
     def ==(other)
@@ -107,11 +107,6 @@ module Assert::Result
       "Fail"
     end
 
-    # override of the base, show the test's context info called_from
-    def trace
-      self.test.context_info.called_from || super
-    end
-
   end
 
   # raised by the 'skip' context helper to break test execution
@@ -133,11 +128,6 @@ module Assert::Result
 
     def name
       "Skip"
-    end
-
-    # override of the base, show the test's context info called_from
-    def trace
-      self.test.context_info.called_from || super
     end
 
   end
@@ -191,42 +181,17 @@ module Assert::Result
     def to_s; self.join("\n"); end
 
     def filtered
-      new_bt = []
-
-      self.each do |line|
-        break if filter_out?(line)
-        new_bt << line
-      end
-
-      new_bt = self.reject { |line| filter_out?(line) } if new_bt.empty?
-      new_bt = self.dup if new_bt.empty?
-
-      self.class.new(new_bt)
+      self.class.new(self.reject { |line| filter_out?(line) })
     end
 
     protected
 
-    # filter a line out if it's an assert lib line
-
+    # filter a line out if it's an assert lib/bin line
     def filter_out?(line)
-      # from minitest (for reference)...
-      # file = File.expand_path __FILE__
-             # if RUBY_VERSION =~ /^1\.9/ then  # bt's expanded, but __FILE__ isn't :(
-             #    File.expand_path __FILE__
-             # elsif  __FILE__ =~ /^[^\.]/ then # assume both relative
-             #   require 'pathname'
-             #   pwd = Pathname.new Dir.pwd
-             #   pn = Pathname.new File.expand_path(__FILE__)
-             #   relpath = pn.relative_path_from(pwd) rescue pn
-             #   pn = File.join ".", relpath unless pn.relative?
-             #   pn.to_s
-             # else                             # assume both are expanded
-             #   __FILE__
-             # end
-
       # './lib' in project dir, or '/usr/local/blahblah' if installed
       assert_lib_path = File.expand_path('../..', __FILE__)
-      line.rindex(assert_lib_path, 0)
+      assert_bin_regex = /bin\/assert\:/
+      line.rindex(assert_lib_path, 0) || line =~ assert_bin_regex
     end
   end
 

test/unit/context_tests.rb
@@ -98,7 +98,7 @@ class Assert::Context
 
     should "create a fail result and set its backtrace" do
       assert_kind_of Assert::Result::Fail, subject
-      assert_equal Factory.context_info_called_from, subject.trace
+      assert_equal subject.backtrace.filtered.first, subject.trace
       assert_kind_of Array, subject.backtrace
     end
 