09150a7fb89092526d427255311ccec43fafba53
Kelly Redding <kelly@kellyredding.com>
2016-05-13 09:40:29 -0500

    wip

--- lib/assert/runner.rb
+++ lib/assert/runner.rb
@@ -17,8 +17,6 @@ module Assert
 
     def run
       self.on_start
-      self.suite.on_start
-      self.view.on_start
 
       if self.single_test?
         self.view.print "Running test: #{self.single_test_file_line}"
@@ -32,51 +30,78 @@ module Assert
       begin
         self.suite.start_time = Time.now
         self.suite.setups.each(&:call)
-        tests_to_run.tap{ self.suite.clear_tests_to_run }.delete_if do |test|
-          self.before_test(test)
-          self.suite.before_test(test)
-          self.view.before_test(test)
-          test.run do |result|
-            self.on_result(result)
-            self.suite.on_result(result)
-            self.view.on_result(result)
-          end
-          self.after_test(test)
-          self.suite.after_test(test)
-          self.view.after_test(test)
+        self.on_run_tests(tests_to_run.tap{ self.suite.clear_tests_to_run }) do |ttr|
+          ttr.delete_if do |test|
+            test_data = test # TODO: test.data (?)
 
-          # always delete `test` from `tests_to_run` since it has been run
-          true
+            self.before_test_run(test_data)
+            test.run do |result|
+              self.on_result(result) # TODO: result.data (?)
+            end
+            self.after_test_run(test_data)
+
+            # always delete `test` from `tests_to_run` since it has been run
+            true
+          end
         end
         self.suite.teardowns.each(&:call)
         self.suite.end_time = Time.now
       rescue Interrupt => err
         self.on_interrupt(err)
-        self.suite.on_interrupt(err)
-        self.view.on_interrupt(err)
         raise(err)
       end
 
-      (self.fail_result_count + self.error_result_count).tap do
-        self.view.on_finish
-        self.suite.on_finish
-        self.on_finish
-      end
+      (self.fail_result_count + self.error_result_count).tap{ self.on_finish }
     end
 
     # Callbacks
 
-    # define callback handlers to do special behavior during the test run. These
-    # will be called by the test runner
+    # override callback handlers to do special behavior during the test run.
 
     def before_load(test_files); end
     def after_load;              end
-    def on_start;                end
-    def before_test(test);       end
-    def on_result(result);       end
-    def after_test(test);        end
-    def on_finish;               end
-    def on_interrupt(err);       end
+
+    def on_start
+      self.suite.on_start
+      self.view.on_start
+    end
+
+    def on_run_tests(tests_to_run)
+      # TODO: setup IO pipes for each callback; fork
+
+      # if in fork...
+      self.on_tests_to_run(tests_to_run)
+
+      # else, TODO: listen on callback pipes for data to process
+    end
+
+    def on_test(test)
+    end
+
+    def before_test_run(test_data)
+      self.suite.before_test(test_data)
+      self.view.before_test(test_data)
+    end
+
+    def on_result(result_data)
+      self.suite.on_result(result_data)
+      self.view.on_result(result_data)
+    end
+
+    def after_test_run(test_data)
+      self.suite.after_test(test_data)
+      self.view.after_test(test_data)
+    end
+
+    def on_finish
+      self.view.on_finish
+      self.suite.on_finish
+    end
+
+    def on_interrupt(err)
+      self.suite.on_interrupt(err)
+      self.view.on_interrupt(err)
+    end
 
     private
 
